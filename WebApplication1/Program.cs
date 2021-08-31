f6630b1cdfef73a7104bf975e00cce0e8e79d2a0
zhao365845726@163.com <zhao365845726@163.com>
2021-08-31 21:38:07 +0800

    升级原站点到3.1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MLTechnology
+namespace WebApplication1
 {
     public class Program
     {
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MLTechnology
+namespace WebApplication1
 {
     public class Startup
     {
